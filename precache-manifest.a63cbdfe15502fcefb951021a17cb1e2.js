cda911e3ba9810e8ec5150e7f172daeca8924d78
Tiago Barbosa <tiagocastrobarbosa@gmail.com>
2020-08-24 16:40:39 -0300

    Updates

---
+++ precache-manifest.a63cbdfe15502fcefb951021a17cb1e2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82bfd1d12dd60e5b6a5b998e7e3d8276",
+    "url": "/flexblog_app/index.html"
+  },
+  {
+    "revision": "9a8d2652e1451260688a",
+    "url": "/flexblog_app/static/js/2.cd2779da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flexblog_app/static/js/2.cd2779da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65395a4de41023701988",
+    "url": "/flexblog_app/static/js/main.d681e74a.chunk.js"
+  },
+  {
+    "revision": "7ef06e634258ac625d94",
+    "url": "/flexblog_app/static/js/runtime-main.c07643f0.js"
+  },
+  {
+    "revision": "fde008955eff85c50eeec1e36364a874",
+    "url": "/flexblog_app/static/media/produtos1.fde00895.jpg"
+  },
+  {
+    "revision": "8797fe0473e192d55eff19a8bbd17b2f",
+    "url": "/flexblog_app/static/media/produtos2.8797fe04.jpg"
+  },
+  {
+    "revision": "428068dc84c8f0abc0437fc34c9a7c73",
+    "url": "/flexblog_app/static/media/produtos3.428068dc.jpg"
+  },
+  {
+    "revision": "2d4a329f29756e6c16da886f5a0d943f",
+    "url": "/flexblog_app/static/media/sobre1.2d4a329f.jpg"
+  },
+  {
+    "revision": "a1f7d0571c3b342f5ca65a7fb814d6f9",
+    "url": "/flexblog_app/static/media/sobre2.a1f7d057.jpg"
+  }
+]);
