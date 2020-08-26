cc9a2cdc168ea9c5cb2e153577e59e05cd29eefd
Tiago Castro Barbosa <tiago.c.barbosa@ericssoninovacao.com.br>
2020-08-26 14:31:38 -0300

    The css of the PRODUCTS section has been added

--- src/components/Main/ProductsSection/index.js
+++ src/components/Main/ProductsSection/index.js
@@ -1,25 +1,55 @@
 import React from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
+
+const Container = styled.section`
+    max-width: 960px;
+    margin: 60px auto;
+`
+
+const Title = styled.h1`
+    font-size: 3em;
+    margin: 0 10px 60px 10px;
+`
+
+const OuterWrapper = styled.div`
+    display: flex;
+    margin: 0 10px;
+    flex-wrap: wrap;
+    align-items: flex-end;
+`
+
+const InnerWrapper = styled.div`
+    flex: 1 1 175px;
+    margin: 0 10px;
+    border-top: 5px solid;
+`
+
+const ProductTitle = styled.h2`
+    color: #A4A4A4;
+    background-color: #F7F7F7;
+    font-size: 3em;
+    padding: 60px 20px;
+`
 
 function ProductsSection() {
   return (
-    <section id='products'>
-        <h1>Produtos</h1>
-        <div>
-            <div>
-                <h2>Purple</h2>
+    <Container id='products'>
+        <Title>Produtos</Title>
+        <OuterWrapper>
+            <InnerWrapper style={{borderTopColor: "#AE81FF"}}>
+                <ProductTitle>Purple</ProductTitle>
                 <img src={require('../../../assets/img/produtos1.jpg')} alt='Produtos 1' />
-            </div>
-            <div>
-                <h2>Pink</h2>
+            </InnerWrapper>
+            <InnerWrapper style={{borderTopColor: "#F9265E"}}>
+                <ProductTitle>Pink</ProductTitle>
                 <img src={require('../../../assets/img/produtos2.jpg')} alt='Produtos 2' />
-            </div>
-            <div>
-                <h2>Blue</h2>
+            </InnerWrapper>
+            <InnerWrapper style={{borderTopColor: "#66D9EB"}}>
+                <ProductTitle>Blue</ProductTitle>
                 <img src={require('../../../assets/img/produtos3.jpg')} alt='Produtos 3' />
-            </div>
-        </div>
-    </section>
+            </InnerWrapper>
+        </OuterWrapper>
+    </Container>
   )
 }
 
