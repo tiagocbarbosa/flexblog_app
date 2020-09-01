f7a1ccb5780d03783b8539cfed9159bf1e9e8714
Tiago Barbosa <tiagocastrobarbosa@gmail.com>
2020-08-31 21:09:35 -0300

    QUALITY section css added

--- src/components/Main/QualitySection/index.js
+++ src/components/Main/QualitySection/index.js
@@ -1,39 +1,72 @@
 import React from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
+import './style.css'
+
+const Container = styled.section`
+  max-width: 960px;
+  margin: 60px auto;
+  display: flex;
+  flex-wrap: wrap;
+`
+const Wrapper = styled.div`
+  flex: 1 1 300px;
+  margin: 20px 10px;
+`
+
+const Title = styled.h2`
+  font-size: 2.25em;
+  color: #A4A4A4;
+  margin-bottom: 20px;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 8px;
+    height: 30px;
+    margin-right: 10px;
+    background-color: blue;
+  }
+`
+
+const Paragraph = styled.p`
+  font-size: 1.25em;
+  line-height: 1.4;
+  color: #7C7C7C;
+`
 
 function QualitySection() {
   return (
-    <section id='quality'>
-        <div>
-          <h2>Inteligente</h2>
-          <p>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições</p>
-        </div>
+    <Container id='quality'>
+        <Wrapper>
+          <Title>Inteligente</Title>
+          <Paragraph>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições</Paragraph>
+        </Wrapper>
 
-        <div>
-          <h2>Compacto</h2>
-          <p>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições inegavelmente apropriadas</p>
-        </div>
+        <Wrapper>
+          <Title>Compacto</Title>
+          <Paragraph>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições inegavelmente apropriadas</Paragraph>
+        </Wrapper>
 
-        <div>
-          <h2>Econômico</h2>
-          <p>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições</p>
-        </div>
+        <Wrapper>
+          <Title>Econômico</Title>
+          <Paragraph>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições</Paragraph>
+        </Wrapper>
 
-        <div>
-          <h2>Transparente</h2>
-          <p>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições</p>
-        </div>
+        <Wrapper>
+          <Title>Transparente</Title>
+          <Paragraph>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições</Paragraph>
+        </Wrapper>
 
-        <div>
-          <h2>Opaco</h2>
-          <p>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições inegavelmente apropriadas</p>
-        </div>
+        <Wrapper>
+          <Title>Opaco</Title>
+          <Paragraph>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições inegavelmente apropriadas</Paragraph>
+        </Wrapper>
 
-        <div>
-          <h2>Sustentável</h2>
-          <p>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições</p>
-        </div>
-    </section>
+        <Wrapper>
+          <Title>Sustentável</Title>
+          <Paragraph>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta positivamente a correta previsão das condições</Paragraph>
+        </Wrapper>
+    </Container>
   )
 }
 
