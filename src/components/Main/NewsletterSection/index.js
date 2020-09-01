407b20b4080946dbc8dba0f1f19ff24cef311827
Tiago Barbosa <tiagocastrobarbosa@gmail.com>
2020-09-01 20:50:19 -0300

    NEWSLETTER section css added

--- src/components/Main/NewsletterSection/index.js
+++ src/components/Main/NewsletterSection/index.js
@@ -1,16 +1,84 @@
 import React from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
+
+const Container = styled.section`
+  background: #f7f7f7;
+	padding: 60px calc((100% - 960px) / 2 );
+	display: flex;
+	flex-wrap: wrap;
+`
+
+const Wrapper = styled.div`
+  flex: 1 1 240px;
+	margin: 0 10px;
+
+  h1 {
+    font-size: 3em;
+    color: #a4a4a4;
+  }
+
+  p{
+    font-size: 1.25em;
+    font-style: italic;
+    max-width: 230px;
+    color: #7c7c7c;
+    margin-bottom: 10px;
+  }
+`
+
+const Form = styled.form`
+  flex: 2 1 260px;
+	margin: 0 10px;
+	align-self: center;
+	display: flex;
+	flex-wrap: wrap;
+
+  input {
+    flex: 3 1 200px;
+    font-size: 1.25em;
+    color: #7c7c7c;
+    font-style: italic;
+    font-family: "Nunito", Helvetica, Arial, sans-serif;
+    border: 4px solid #2c70ff;
+    background: transparent;
+    padding: 15px 10px;
+    margin: 0px;
+    border-radius: 0px;
+  }
+
+  button {
+    flex: 1 1 200px;
+    font-family: "Nunito", Helvetica, Arial, sans-serif;
+    font-size: 2.25em;
+    font-weight: bold;
+    text-transform: uppercase;
+    background: #2c70ff;
+    color: white;
+    margin: 0px;
+    cursor: pointer;
+    border: 0px;
+    padding: 15px 10px;
+
+    @media (max-width: 600px) {
+	    & {
+		  font-size: 1.5em;
+	    }
+    }
+  }
+`
 
 function NewsletterSection() {
   return (
-    <section id='contact'>
-      <div>
+    <Container id='contact'>
+      <Wrapper>
         <h1>Newsletter</h1>
         <p>assine e fique por dentro das novidades</p>
-      </div>
-      <input type='text' placeholder='Seu e-mail'></input>
-      <button type='submit'>Assinar</button>
-    </section>
+      </Wrapper>
+      <Form>
+        <input type='text' placeholder='Seu e-mail'></input>
+        <button type='submit'>Assinar</button>        
+      </Form>
+    </Container>
   )
 }
 
