d36b9e7d739d948b307bb19b3e547f1cd233d69b
Tiago Barbosa <tiagocastrobarbosa@gmail.com>
2020-09-01 22:14:38 -0300

    FOOTER section css added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,35 @@
 import React from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
+
+const Container = styled.footer`
+  display: flex;
+  background: #2c70ff;
+  height: 200px;
+  align-items: center;
+  
+  /* ...or this style
+  background: #2c70ff;
+  padding: 60px; */
+  
+  p {
+    flex: 1;
+    text-align: center;
+    font-size: 2.25em;
+    color: #122ab2;
+
+    /* ...or this style
+    text-align: center;
+    font-size: 2.25em;
+    color: #122ab2;
+    margin-bottom: 10px; */
+  }
+`
 
 function Footer() {
   return (
-    <footer>
+    <Container>
         <p>FlexBlog © Todos os direitos reservados.</p>
-    </footer>
+    </Container>
   )
 }
 
